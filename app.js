c95920ede85d0447364b54f6e5c68280f7270b0b
shivamkr250199 <shivamshandaliya1998@gmail.com>
2025-05-11 18:55:03 +0530

    commit initial files

---
+++ app.js
@@ -0,0 +1,9 @@
+ const emoji=document.querySelector("#emoji");
+ const emojis=[
+    "😎","😋 ","😅 ", "😃 ","🤣 ","😂 ","😁","😀 ",
+    "😍","😚","🤗","🤩"
+ ];
+ emoji.addEventListener("mouseover",()=>{
+    emoji.innerHTML=emojis[Math.floor(Math.random()*emojis.length)];
+
+ });
